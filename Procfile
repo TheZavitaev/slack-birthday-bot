a2812bd581518a9307d4e1a91ee8108a1e655f24
Aleksandr Bebyakov <alex0181@yandex.ru>
2020-07-12 23:28:41 +0400

    add events api

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn starter_bot.wsgi --log-file -
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status, mixins, viewsets
+from django.conf import settings
 
-# Create your views here.
+
+SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
+
+
+class SlackEventsSubscriptionViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    def create(self, request, *args, **kwargs):
+        slack_message = request.data
+        if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if slack_message.get('type') == 'url_verification':
+            return Response(data=slack_message, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
