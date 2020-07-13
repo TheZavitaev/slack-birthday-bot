1f8b05f7712be0707d28ee5bf4d3ba856961d569
Dmitry Morozov <inood@yandex.ru>
2020-07-13 22:11:29 +0700

    Модели для БД

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Gift, Staff, Team, Wishlist
 
-# Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'birth_date', 'sex', 'slack_id')
+
+
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'employee_name')
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teamlead')
+
+
+admin.site.register(Gift, GiftAdmin)
+admin.site.register(Wishlist, WishlistAdmin)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Team, TeamAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+	name = models.CharField(max_length=250)
+	birth_date = models.DateTimeField()
+	sex = models.BooleanField(null=True)
+	slack_id = models.CharField(max_length=250)
+	is_admin = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
+
+
+class Team(models.Model):
+	teamlead = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='teamlead')
+	teammate = models.ManyToManyField(Staff, related_name='teammate')
+
+	def __str__(self):
+		return f'комманда: {self.teamlead}'
+
+
+class Gift(models.Model):
+	name = models.CharField(max_length=250)
+
+
+class Wishlist(models.Model):
+	employee_name = models.ForeignKey(Staff, on_delete=models.CASCADE)
+	gift = models.ManyToManyField(Gift)
+
+	def __str__(self):
+		return f'{self.employee_name} не отказался бы от {self.gift}'
+
+
