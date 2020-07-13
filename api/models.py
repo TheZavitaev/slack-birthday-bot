a874c0a9ccd54ef0383b81e25ae053f97cddfd2c
Dmitry Morozov <inood@yandex.ru>
2020-07-13 22:11:29 +0700

    Модели для БД

--- api/models.py
+++ api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+	name = models.CharField(max_length=250)
+	birth_date = models.DateTimeField()
+	sex = models.BooleanField(null=True)
+	slack_id = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.name
+
+
+class Team(models.Model):
+	teamlead = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='teamlead')
+	teammate = models.ManyToManyField(Staff, related_name='teammate')
+
+	def __str__(self):
+		return f'{self.teamlead} и его команда'
+
+
+class Gift(models.Model):
+	name = models.CharField(max_length=250)
+
+
+class Wishlist(models.Model):
+	employee_name = models.ForeignKey(Staff, on_delete=models.CASCADE)
+	gift = models.ManyToManyField(Gift)
+
+	def __str__(self):
+		return f'{self.employee_name} не отказался бы от {self.gift}'
+
+
