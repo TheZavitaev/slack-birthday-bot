33226f710d776c3cd9dc74f928c9e58d54064e88
Aleksandr Bebyakov <alex0181@yandex.ru>
2020-07-15 12:36:58 +0400

    add bot engine

--- api/models.py
+++ api/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Staff(models.Model):
 	name = models.CharField(max_length=250)
-	birth_date = models.DateTimeField()
+	birth_date = models.DateTimeField(null=True, blank=True)
 	sex = models.BooleanField(null=True)
-	slack_id = models.CharField(max_length=250)
+	slack_id = models.CharField(max_length=250, null=False, blank=False, unique=True)
+	activated = models.BooleanField(null=True)
 
 	def __str__(self):
 		return self.name
@@ -31,3 +32,11 @@ class Wishlist(models.Model):
 		return f'{self.employee_name} не отказался бы от {self.gift}'
 
 
+class Interaction(models.Model):
+	INTERACTION_CHOICES = (
+		('UJF', 'user_join_form'),
+	)
+	message_ts = models.CharField(max_length=250, null=False, blank=False)
+	kind = models.CharField(max_length=3, choices=INTERACTION_CHOICES, default='UJF')
+	values_json = models.CharField(max_length=250, null=False, blank=False)
+	user = models.ForeignKey(Staff, on_delete=models.CASCADE)
