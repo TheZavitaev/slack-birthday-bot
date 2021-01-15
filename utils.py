4912d74900e35f41c38fc455404efaf5173317f3
Zavitaev Oleg <zavitaev@pm.com>
2021-01-15 15:29:46 +0300

    Косметический рефакторинг кода

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import datetime
+
+
 def get_or_create(session, model, **kwargs):
+    """Получает экземпляр модели по запросу, если такового нет - создает его"""
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
         return instance
@@ -9,8 +13,20 @@ def get_or_create(session, model, **kwargs):
         return instance
 
 
-def bool_convert(string):
+def bool_convert(string: str) -> bool:
+    """Конвертирует строку в Bool"""
     if string == 'True':
         return True
     else:
         return False
+
+
+def get_channel_id(data: dict) -> str:
+    """Возвращает id канала из payload"""
+    channel = data['channel']
+    return channel
+
+
+def convert_birthday(birthday):
+    birthday = datetime.date(*tuple(map(int, birthday.split('-'))))
+    return birthday
