2d16588339c63f48ed7872c2eb889578cb8c09eb
Zavitaev Oleg <zavitaev@pm.com>
2021-01-16 17:18:02 +0300

    Костыльное решение для вывода 3х ближайших именинников

--- context.py
+++ context.py
@@ -1,61 +1,73 @@
-update_home_tab_context = {
-    "type": "home",
-    "callback_id": "home_view",
-    "blocks": [
-        {
-            "type": "section",
-            "block_id": "birthday",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Выберите дату своего рождения"
-            },
-            "accessory": {
-                "type": "datepicker",
-                "initial_date": "1990-01-01",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Select a date",
-                },
-                "action_id": "datepicker-birthday"
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "section",
-            "block_id": "congratulate",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Хотите ли вы получать поздравления в общем канале?"
-            },
-            "accessory": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "выберите вариант",
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "да",
-                        },
-                        "value": 'True'
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "нет",
-                        },
-                        "value": 'False'
-                    }
-                ],
-                "action_id": "congratulate_on_the_general_channel"
-            }
-        },
-    ]
-}
+# update_home_tab_context = {
+#     "type": "home",
+#     "callback_id": "home_view",
+#     "blocks": [
+#         {
+#             "type": "section",
+#             "block_id": "birthday",
+#             "text": {
+#                 "type": "mrkdwn",
+#                 "text": "Выберите дату своего рождения"
+#             },
+#             "accessory": {
+#                 "type": "datepicker",
+#                 "initial_date": "1990-01-01",
+#                 "placeholder": {
+#                     "type": "plain_text",
+#                     "text": "Select a date",
+#                 },
+#                 "action_id": "datepicker-birthday"
+#             }
+#         },
+#         {
+#             "type": "divider"
+#         },
+#         {
+#             "type": "section",
+#             "block_id": "congratulate",
+#             "text": {
+#                 "type": "mrkdwn",
+#                 "text": "Хотите ли вы получать поздравления в общем канале?"
+#             },
+#             "accessory": {
+#                 "type": "static_select",
+#                 "placeholder": {
+#                     "type": "plain_text",
+#                     "text": "выберите вариант",
+#                 },
+#                 "options": [
+#                     {
+#                         "text": {
+#                             "type": "plain_text",
+#                             "text": "да",
+#                         },
+#                         "value": 'True'
+#                     },
+#                     {
+#                         "text": {
+#                             "type": "plain_text",
+#                             "text": "нет",
+#                         },
+#                         "value": 'False'
+#                     }
+#                 ],
+#                 "action_id": "congratulate_on_the_general_channel"
+#             }
+#         },
+#
+#         {
+#             "type": "divider"
+#         },
+#         {
+#             "type": "context",
+#             "elements": [
+#                 {
+#                     "type": "plain_text",
+#                     "text": "Author: K A Applegate",
+#                 }
+#             ]}
+#     ]
+# }
 
 update_home_tab_context_admin = {
     "type": "home",
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-import datetime
+import datetime as dt
+from itertools import groupby
+
+from sqlalchemy import extract
+
+from bot import User, db
 
 
 def get_or_create(session, model, **kwargs):
@@ -27,6 +32,31 @@ def get_channel_id(data: dict) -> str:
     return channel
 
 
-def convert_birthday(birthday):
-    birthday = datetime.date(*tuple(map(int, birthday.split('-'))))
+def convert_birthday(birthday: str):
+    birthday = dt.date(*tuple(map(int, birthday.split('-'))))
     return birthday
+
+
+def get_birthday_persons():
+    date_now = dt.datetime(dt.datetime.today().year, dt.datetime.today().month,
+                           dt.datetime.today().day)
+
+    persons = []
+
+    bday_sort = User.query.group_by(
+        extract('month', User.birthday),
+        extract('day', User.birthday)).filter(
+        extract('month', User.birthday) >= dt.datetime.today().month).all()
+
+    for p in bday_sort:
+        replace_date = p.birthday.replace(year=date_now.year)
+        if replace_date > date_now.date():
+            persons.append(p)
+
+    person_1 = persons[0]
+    bday_month_1 = person_1.birthday.strftime("%b")
+    person_2 = persons[1]
+    bday_month_2 = person_2.birthday.strftime("%b")
+    person_3 = persons[2]
+    bday_month_3 = person_3.birthday.strftime("%b")
+    return person_1, person_2, person_3, bday_month_1, bday_month_2, bday_month_3
