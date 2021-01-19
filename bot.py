6345c1cfa9c7eeaea9cd5cb5e92e411ea9ca4c52
Zavitaev Oleg <zavitaev@pm.com>
2021-01-19 13:28:22 +0300

    Добавил отправку поздравления по кнопке, прикрутил гифки

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+import json
 import os
 import re
 
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
@@ -12,8 +13,8 @@ import utils
 load_dotenv()
 
 app = App(
-    token=os.getenv("SLACK_BOT_TOKEN"),
-    signing_secret=os.getenv("SLACK_SIGNING_SECRET")
+    token=os.getenv('SLACK_BOT_TOKEN'),
+    signing_secret=os.getenv('SLACK_SIGNING_SECRET')
 )
 handler = SlackRequestHandler(app)
 
@@ -54,161 +55,270 @@ class Team(db.Model):
     channel_id = db.Column(db.String(255), nullable=False)
 
 
-@app.command("/echo")
-def repeat_text(ack, say, command):
-    # Acknowledge command request
-    ack()
-    say(f"{command['text']}")
-
-
-@app.message(re.compile("(hi|hello|hey|привет|:wave:|даров|дратути)"))
+@app.message(re.compile('(hi|hello|hey|привет|:wave:|даров|дратути)'))
 def say_hello_regex(say, context, logger):
     try:
         greeting = context['matches'][0]
         slack_id = context['user_id']
-        say(f"{greeting}, <@{slack_id}> how are you?")
+        say(f'{greeting}, <@{slack_id}> how are you?')
 
     except Exception as e:
-        logger.error(f"Error say_hello_regex: {e}")
+        logger.error(f'Error say_hello_regex: {e}')
 
 
-@app.event("app_home_opened")
+@app.event('team_join')
+def ask_for_introduction(event, say):
+    welcome_channel_id = utils.get_channel_id(event)
+    slack_id = event["user"]["id"]
+    person = utils.get_or_create(db.session, User, slack_id=slack_id)
+    blocks = [
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": "Выбери дату своего рождения"},
+            "accessory": {
+                "type": "datepicker",
+                "action_id": "datepicker-birthday",
+                "initial_date": "1990-01-01",
+                "placeholder": {"type": "plain_text", "text": "Выбери дату"}
+            }
+        }
+    ]
+    say(
+        blocks=blocks,
+        text=f'Добро пожаловать, <@{slack_id}>! 🎉 Я поздработ!',
+        channel=welcome_channel_id
+    )
+
+
+@app.event('app_home_opened')
 def update_home_tab(client, event, logger):
     """Вью Home page"""
     try:
-        slack_id = event["user"]
+        slack_id = event['user']
         person = utils.get_or_create(db.session, User, slack_id=slack_id)
+        # Показывает дату рождения юзера в качестве заполнителя
+        try:
+            initial_date = person.birthday
+        except:
+            initial_date = '1990-01-01'
+
+        # Получаем 3х "ближайших" именинников
         persons = utils.get_birthday_persons()
         birth_month = {}
-        # Получаем короткое обозначение месяца дня рождения.
+        # Получаем короткое обозначение месяца дня рождения в виде словаря.
         for p in persons:
             month = utils.get_birthday_month(p)
             birth_month[p] = month
 
-
-        # if person.is_admin:
-        #     client.views_publish(
-        #         user_id=person.slack_id,
-        #         view=update_home_tab_context_admin)
-        # else:
         client.views_publish(
-                user_id=person.slack_id,
-                view={
-                    "type": "home",
-                    "blocks": [
-                        {
-                            "type": "section",
-                            "block_id": "birthday",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "Выберите дату своего рождения"
-                            },
-                            "accessory": {
-                                "type": "datepicker",
-                                "initial_date": "1990-01-01",
-                                "placeholder": {
-                                    "type": "plain_text",
-                                    "text": "Select a date"
-                                },
-                                "action_id": "datepicker-birthday"
-                            }
+            user_id=person.slack_id,
+            view={
+                "type": "home",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "block_id": "birthday",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "Выберите дату своего рождения"
                         },
-                        {
-                            "type": "divider"
-                        },
-                        {
-                            "type": "section",
-                            "block_id": "congratulate",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "Хотите ли вы получать поздравления в общем канале?"
-                            },
-                            "accessory": {
-                                "type": "static_select",
-                                "placeholder": {
-                                    "type": "plain_text",
-                                    "text": "выберите вариант"
-                                },
-                                "options": [
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "да"
-                                        },
-                                        "value": "True"
-                                    },
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "нет"
-                                        },
-                                        "value": "False"
-                                    }
-                                ],
-                                "action_id": "congratulate_on_the_general_channel"
-                            }
-                        },
-                        {
-                            "type": "divider"
-                        },
-                        {
-                            "type": "header",
-                            "text": {
+                        "accessory": {
+                            "type": "datepicker",
+                            "initial_date": f'{initial_date}',
+                            "placeholder": {
                                 "type": "plain_text",
-                                "text": "Скоро дни рождения у:"
-                            }
-                        },
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "image",
-                                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-                                    "alt_text": "cute cat"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": f"<@{persons[0].slack_id}> отмечает день рождение {persons[0].birthday.day} {birth_month[persons[0]]}"
-                                }
-                            ]
-                        },
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "image",
-                                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-                                    "alt_text": "cute cat"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": f"<@{persons[1].slack_id}> отмечает день рождение {persons[1].birthday.day} {birth_month[persons[1]]}"
-                                }
-                            ]
-                        },
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "image",
-                                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-                                    "alt_text": "cute cat"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": f"<@{persons[2].slack_id}> отмечает день рождение {persons[2].birthday.day} {birth_month[persons[2]]}"
-                                }
-                            ]
+                                "text": "Select a date"
+                            },
+                            "action_id": "datepicker-birthday"
                         }
-                    ]
-                }
-            )
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "block_id": "congratulate",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "Хотите ли вы получать поздравления в общем канале?"
+                        },
+                        "accessory": {
+                            "type": "static_select",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "выберите вариант"
+                            },
+                            "options": [
+                                {
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "да"
+                                    },
+                                    "value": "True"
+                                },
+                                {
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "нет"
+                                    },
+                                    "value": "False"
+                                }
+                            ],
+                            "action_id": "congratulate_on_the_general_channel"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Скоро дни рождения у:"
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "image",
+                                "image_url": "https://mygamehunter.ru/images/thumbnail/147702/550",
+                                "alt_text": "cute cat"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"<@{persons[0].slack_id}> отмечает день рождение {persons[0].birthday.day} {birth_month[persons[0]]}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "image",
+                                "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                                "alt_text": "cute cat"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"<@{persons[1].slack_id}> отмечает день рождение {persons[1].birthday.day} {birth_month[persons[1]]}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "image",
+                                "image_url": "https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/999/UP2538-CUSA05620_00-ASIA000000000148/1578133265000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000",
+                                "alt_text": "cute cat"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"<@{persons[2].slack_id}> отмечает день рождение {persons[2].birthday.day} {birth_month[persons[2]]}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Отправь приветственное сообщение"
+                                },
+                                "value": "click_me_123",
+                                "action_id": "send_greeting_message"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Отправь оздравление именинникам"
+                                },
+                                "value": "congrat_button",
+                                "action_id": "send_congratulations"
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
 
     except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
+        logger.error(f'Error publishing home tab: {e}')
 
 
-@app.action("datepicker-birthday")
+@app.action('send_greeting_message')
+def send_greeting_message(ack, say):
+    ack()
+    blocks = [
+        {
+            "type": "section",
+            "block_id": "birthday",
+            "text": {"type": "mrkdwn", "text": "Всем привет! Я поздработ! "
+                                               "Введи дату своего рождения и"
+                                               " никто про тебя не забудет "
+                                               ":)"},
+            "accessory": {
+                "type": "datepicker",
+                "action_id": "datepicker-birthday",
+                "initial_date": "1990-01-01",
+                "placeholder": {"type": "plain_text", "text": "Выбери дату"}
+            }
+        }
+    ]
+    say(
+        blocks=blocks,
+        channel='C018QT2BV5X'  # CHANNEL_ID
+    )
+
+
+@app.action('send_congratulations')
+def send_greeting_message(ack, say):
+    ack()
+    birthday_persons = utils.find_birthday_person()
+
+    i = 0
+    while i != len(birthday_persons):
+        for person in birthday_persons:
+            gif = utils.get_gif()
+            blocks = [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"Сегодня день рождение у"
+                                f" <@{person.slack_username}>!",
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Мы ему от всей души желаем морковки, хлеба и вина"
+                    }
+                },
+                {
+                    "type": "image",
+                    "image_url": f"{gif}",
+                    "alt_text": "inspiration"
+                }
+            ]
+            say(
+                blocks=blocks,
+                channel='C018QT2BV5X'  # CHANNEL_ID
+            )
+            i += 1
+
+
+@app.action('datepicker-birthday')
 def get_birthday(ack, body, logger):
     """Достает дату рождения"""
     try:
@@ -216,17 +326,23 @@ def get_birthday(ack, body, logger):
         slack_id = body['user']['id']
         person = utils.get_or_create(db.session, User, slack_id=slack_id)
         person.slack_username = body['user']['username']
-        birthday = \
-            body['view']['state']['values']['birthday']['datepicker-birthday'][
-                'selected_date']
+
+        try:
+            birthday = \
+                body['view']['state']['values']['birthday'][
+                    'datepicker-birthday'][
+                    'selected_date']
+        except:
+            birthday = body['actions'][0]['selected_date']
+
         person.birthday = utils.convert_birthday(birthday)
         db.session.commit()
 
     except Exception as e:
-        logger.error(f"Error get birthday: {e}")
+        logger.error(f'Error get birthday: {e}')
 
 
-@app.action("congratulate_on_the_general_channel")
+@app.action('congratulate_on_the_general_channel')
 def get_congratulate(ack, body, logger):
     """Достает согласие на поздравление"""
     try:
@@ -239,10 +355,10 @@ def get_congratulate(ack, body, logger):
         db.session.commit()
 
     except Exception as e:
-        logger.error(f"Error send congratulate message: {e}")
+        logger.error(f'Error send congratulate message: {e}')
 
 
-@app.message("get_data")
+@app.message('get_data')
 def get_data(message, say, logger):
     """Тестовая ручка, чтобы получать инфу о канале и пользователе"""
     try:
@@ -266,7 +382,7 @@ def get_data(message, say, logger):
         else:
             is_teamlead = 'не тимлид'
 
-        say(text=f"Приветики <@{person.slack_id}>! Мы в канале: {channel}. "
+        say(text=f"Приветики, <@{person.slack_id}>! Мы в канале: {channel}. "
                  f"Твой id - {slack_id} у тебя ДР - {birthday}. "
                  f"Ты {is_admin}. "
                  f"Получать поздравления ты {is_congratulate}. "
@@ -283,7 +399,10 @@ def slack_events():
 
 @flask_app.route("/")
 def index():
-    return 'Hello, World! This help page'
+    pagetitle = 'HomePage'
+    return render_template('index.html',
+                           mytitle=pagetitle,
+                           mycontent="Hello World, i'm happy bot")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import random
 
 from sqlalchemy import extract
 
@@ -37,6 +38,7 @@ def convert_birthday(birthday: str):
 
 
 def get_birthday_persons() -> list:
+    """Получаем отсортированный по дню и месяцу список именинников"""
     date_now = dt.datetime(dt.datetime.today().year, dt.datetime.today().month,
                            dt.datetime.today().day)
 
@@ -56,5 +58,27 @@ def get_birthday_persons() -> list:
 
 
 def get_birthday_month(person) -> str:
+    """Получаем месяц рождения"""
     bday_month = person.birthday.strftime("%b")
     return bday_month
+
+
+def find_birthday_person():
+    bday_qs = User.query.filter(
+        extract('month', User.birthday) == dt.datetime.today().month,
+        extract('day', User.birthday) == dt.datetime.today().day).all()
+    return bday_qs
+
+
+def get_gif():
+    gifs = [
+        'https://media.giphy.com/media/lZqlpPlT9llVm/giphy.gif',
+        'https://media.giphy.com/media/26FPzWoJlFvXyiZ6E/giphy.gif',
+        'https://media.giphy.com/media/9hqsyNVlf0DOU/giphy.gif',
+        'https://media.giphy.com/media/45gODt1krqCOI/giphy.gif',
+        'https://media.giphy.com/media/ToMjGpM3hk5UL5UMQ4o/giphy.gif',
+        'https://media.giphy.com/media/W0rfEyF1UeEda/giphy.gif',
+        'https://media.giphy.com/media/WUO8fZQmigr4aiqmgl/giphy.gif',
+        'https://media.giphy.com/media/3ryOqmAw0s1Ve/giphy.gif',
+    ]
+    return random.choice(gifs)
